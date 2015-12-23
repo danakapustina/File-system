a798ae2778cae764d9867a71072e888db0a584b5
danakapustina <dana-kapustina@mail.ru>
2015-12-23 23:49:19 +0300

    Добавлены функции write .mkdir, mknod, rename, rmdir, unlink, truncate

--- filesystem.c
+++ filesystem.c
@@ -3,34 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
-#ifndef FSACTIONS
-#define FSACTIONS
-#include <fuse.h>
-#define _FILE_OFFSET_BITS 64
-#define FUSE_USE_VERSION 26
 
-
-#pragma pack(1)
-
-
-
-#ifdef DEBUG
-#define TRACE printf("[DEBUG] FILE:%s LINE:%d\n", __FILE__, __LINE__);
-
-#else
-
-#define TRACE
-#endif
-
-#define FILESYSTEM "filesystem"
-#define NUMBER_OF_ROOT_BLOCK 0
-#define SIZE_OF_BLOCK 4096
-#define NODE_NAME_MAX_SIZE 256
-#define BLOCK_STATUS_OFFSET 0
-#define NODE_NAME_OFFSET (BLOCK_STATUS_OFFSET + sizeof(char))
-#define NODE_STAT_OFFSET (NODE_NAME_OFFSET + NODE_NAME_MAX_SIZE)
-#define NODE_CONTENT_OFFSET (NODE_STAT_OFFSET + sizeof(stat_t))
-#define NODE_CONTENT_MAX_SIZE (size_of_block - NODE_CONTENT_OFFSET)
 
 // получаем атрибуты файла
 int my_getattr(const char *path, struct stat *stbuf);
@@ -41,7 +14,19 @@ int my_open(const char *path, struct fuse_file_info *fi);
 // читаем данные из открытого файла
 int my_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi);
 // предоставляет возможность записать в открытый файл
-
+int my_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi);
+// создаём папку
+int my_mkdir(const char *path, mode_t mode);
+// создаём файл
+int my_mknod(const char *path, mode_t mode, dev_t dev);
+// переименование
+int my_rename(const char *old_path, const char *new_path);
+// удалям папку
+int my_rmdir(const char *path);
+// удаляем файл
+int my_unlink(const char *path);
+// изменить размер файла
+int my_truncate(const char *path, off_t size);
 
 int my_getattr(const char *path, struct stat *stbuf)
 {
@@ -145,6 +130,220 @@ int my_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_
 }
 
 
+int my_write(const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
+{
+    int result = -ENOENT;
+    char **node_names = split_path(path);
+    if (node_names != NULL)
+    {
+        int number = search_inode(number_of_root_block, node_names);
+        if (number >= 0)
+        {
+            inode_t *file = (inode_t *)get_block(number);
+            if (file != NULL)
+            {
+                if (file->status == BLOCK_STATUS_FILE)
+                {
+                    if (offset < NODE_CONTENT_MAX_SIZE)
+                    {
+                        if (offset + size > NODE_CONTENT_MAX_SIZE)
+                        {
+                            size = NODE_CONTENT_MAX_SIZE - offset;
+                        }
+                        memcpy(file->content + offset, buf, size);
+                        if (file->stat.st_size < offset + size)
+                        {
+                            file->stat.st_size = offset + size;
+                        }
+                        if (write_block(number, file) == 0)
+                        {
+                            result = size;
+                        }
+                    }
+                    else
+                    {
+                        result = 0;
+                    }
+                }
+                destroy_block(file);
+            }
+        }
+        destroy_node_names(node_names);
+    }
+    return result;
+}
+
+
+int my_mkdir(const char *path, mode_t mode)
+{
+    int result = -ENOENT;
+    char **node_names = split_path(path);
+    if (node_names != NULL)
+    {
+        char *name = exclude_last_node_name(node_names);
+        if (name != NULL)
+        {
+            int folder_number = search_inode(number_of_root_block, node_names);
+            if (folder_number >= 0)
+            {
+                int new_folder = create_folder(name, mode);
+                if (new_folder >= 0 && add_inode_to_folder(folder_number, new_folder) == 0)
+                {
+                    result = 0;
+                }
+            }
+            destroy_name(name);
+        }
+        destroy_node_names(node_names);
+    }
+    return result;
+}
+
+
+int my_mknod(const char *path, mode_t mode, dev_t dev)
+{
+    int result = -ENOENT;
+    char **node_names = split_path(path);
+    if (node_names != NULL)
+    {
+        char *name = exclude_last_node_name(node_names);
+        if (name != NULL)
+        {
+            int folder_number = search_inode(number_of_root_block, node_names);
+            if (folder_number >= 0)
+            {
+                int new_file = create_file(name, mode, dev);
+                if (new_file >= 0 && add_inode_to_folder(folder_number, new_file) == 0)
+                {
+                    result = 0;
+                }
+            }
+            destroy_name(name);
+        }
+        destroy_node_names(node_names);
+    }
+    return result;
+}
+
+
+int my_rename(const char *old_path, const char *new_path)
+{
+    int result = -ENOENT;
+    char **old_node_names = split_path(old_path);
+    if (old_node_names != NULL)
+    {
+        char **new_node_names = split_path(new_path);
+        if (new_node_names != NULL)
+        {
+            char *old_name = exclude_last_node_name(old_node_names);
+            if (old_name != NULL)
+            {
+                char *new_name = exclude_last_node_name(new_node_names);
+                if (new_name != NULL)
+                {
+                    int old_folder_number = search_inode(number_of_root_block, old_node_names);
+                    int new_folder_number = search_inode(number_of_root_block, new_node_names);
+                    int node_number = search_inode_in_folder(old_folder_number, old_name);
+                    remove_node_from_folder(old_folder_number, node_number);
+                    add_inode_to_folder(new_folder_number, node_number);
+                    set_inode_name(node_number, new_name);
+                    result = 0;
+                    destroy_name(new_name);
+                }
+                destroy_name(old_name);
+            }
+            destroy_node_names(new_node_names);
+        }
+        destroy_node_names(old_node_names);
+    }
+    return result;
+}
+
+
+int my_rmdir(const char *path)
+{
+    int result = -ENOENT;
+    char **node_names = split_path(path);
+    if (node_names != NULL)
+    {
+        char *name = exclude_last_node_name(node_names);
+        if (name != NULL)
+        {
+            int folder_number = search_inode(number_of_root_block, node_names);
+            if (folder_number >= 0)
+            {
+                int node_number = search_inode_in_folder(folder_number, name);
+                if (node_number >= 0)
+                {
+                    if (remove_node_from_folder(folder_number, node_number) == 0 && remove_block(node_number) == 0)
+                    {
+                        result = 0;
+                    }
+                }
+            }
+            destroy_name(name);
+        }
+        destroy_node_names(node_names);
+    }
+    return result;
+}
+
+
+int my_unlink(const char *path)
+{
+    int result = -ENOENT;
+    char **node_names = split_path(path);
+    if (node_names != NULL)
+    {
+        char *name = exclude_last_node_name(node_names);
+        if (name != NULL)
+        {
+            int folder_number = search_inode(number_of_root_block, node_names);
+            if (folder_number >= 0)
+            {
+                int node_number = search_inode_in_folder(folder_number, name);
+                if (node_number >= 0)
+                {
+                    if (remove_node_from_folder(folder_number, node_number) == 0 && remove_block(node_number) == 0)
+                    {
+                        result = 0;
+                    }
+                }
+            }
+            destroy_name(name);
+        }
+        destroy_node_names(node_names);
+    }
+    return result;
+}
+
+
+int my_truncate(const char *path, off_t size)
+{
+    int result = -ENOENT;
+    char **node_names = split_path(path);
+    if (node_names != NULL)
+    {
+        int number = search_inode(number_of_root_block, node_names);
+        if (number >= 0)
+        {
+            stat_t stat;
+            if (get_inode_stat(number, &stat) == 0)
+            {
+                if (size <= NODE_CONTENT_MAX_SIZE)
+                {
+                    stat.st_size = size;
+                    if (set_inode_stat(number, &stat) == 0)
+                    {
+                        result = 0;
+                    }
+                }
+            }
+        }
+        destroy_node_names(node_names);
+    }
+    return result;
+}
 
 
 // структура определенных мной операций
@@ -154,11 +353,21 @@ struct fuse_operations my_oper =
     .readdir    = my_readdir,
     .open       = my_open,
     .read       = my_read,
-    
+    .write      = my_write,
+    .mkdir      = my_mkdir,
+    .mknod      = my_mknod,
+    .rename     = my_rename,
+    .rmdir      = my_rmdir,
+    .unlink     = my_unlink,
+    .truncate   = my_truncate,
 };
 
 int main(int argc, char *argv[])
 {
-   
+    if (init() != 0)
+    {
+        printf("initialization error\n");
+        return -1;
+    }
     return fuse_main(argc, argv, &my_oper, NULL);
 }
--- fsactions.c
+++ fsactions.c
@@ -47,7 +47,6 @@ int create_root()
 }
 
 
-
 void *create_block()
 {
     return calloc(size_of_block, sizeof(char));
@@ -58,6 +57,7 @@ void destroy_block(void *block)
 {
     free(block);
 }
+
 
 int read_block(int number, void *block)
 {
@@ -86,6 +86,35 @@ int write_block(int number, void *block)
     return result;
 }
 
+
+int search_free_block()
+{
+    int number = number_of_root_block + 1;
+    char status;
+    int read_result;
+    while (TRUE)
+    {
+        if (lseek(filesystem_fd,  size_of_block * number, SEEK_SET) < 0)
+        {
+            number = -1;
+            break;
+        }
+        read_result = read(filesystem_fd, &status, sizeof(char));
+        if (read_result < 0)
+        {
+            number = -1;
+            break;
+        }
+        if (read_result == 0 || status == BLOCK_STATUS_FREE)
+        {
+            break;
+        }
+        number++;
+    }
+    return number;
+}
+
+
 void *get_block(int number)
 {
     void *block = NULL;
@@ -101,10 +130,70 @@ void *get_block(int number)
     return block;
 }
 
+
+int set_block_status(int number, char status)
+{
+    int result = -1;
+    if (number >= 0 && lseek(filesystem_fd, size_of_block * number + BLOCK_STATUS_OFFSET, SEEK_SET) >= 0)
+    {
+        if (write(filesystem_fd, &status, sizeof(char)) == sizeof(char))
+        {
+            result = 0;
+        }
+    }
+    return result;
+}
+
+
+int remove_block(int number)
+{
+    int result = 0;
+    int status = get_block_status(number);
+    switch (status)
+    {
+        case BLOCK_STATUS_FREE:
+            break;
+        case BLOCK_STATUS_FOLDER:
+            remove_folder(number);
+            break;
+        case BLOCK_STATUS_FILE:
+            remove_file(number);
+            break;
+        default:
+            result = -1;
+            break;
+    }
+    return result;
+}
+
+
+int search_inode(int node_number, char **node_names)
+{
+    int result = -1;
+    if (node_number >= 0 && node_names != NULL)
+    {
+        if (*node_names == NULL)
+        {
+            result = node_number;
+        }
+        else
+        {
+            int next_node_number = search_inode_in_folder(node_number, *node_names);
+            if (next_node_number > 0)
+            {
+                result = search_inode(next_node_number, node_names + 1);
+            }
+        }
+    }
+    return result;
+}
+
+
 int remove_file(int number)
 {
     return set_block_status(number, BLOCK_STATUS_FREE);
 }
+
 
 int remove_folder(int number)
 {
@@ -184,6 +273,145 @@ int create_file(const char *name, mode_t mode, dev_t dev)
     }
     return number;
 }
+
+
+char **split_path(const char *path)
+{
+    char **result = NULL;
+    int path_size = strlen(path) + 1;
+    char *copy_path = (char *)malloc(path_size);
+    if (copy_path != NULL)
+    {
+        memcpy(copy_path, path, path_size);
+        int depth = 0;
+        int i = 0;
+        while (copy_path[i] != '\0')
+        {
+            if (copy_path[i] == '/')
+            {
+                depth++;
+                copy_path[i] = '\0';
+            }
+            i++;
+        }
+        if (copy_path[i - 1] == '\0')
+        {
+            depth--;
+        }
+        result = (char **)malloc(sizeof(char **) * (depth + 1));
+        if (result != NULL)
+        {
+            i = 0;
+            int j = 0;
+            while (j < depth)
+            {
+                while (copy_path[i++] != '\0');
+                result[j++] = create_name(copy_path + i);
+            }
+            result[j] = NULL;
+        }
+        free(copy_path);
+    }
+    return result;
+}
+
+
+int get_block_status(int number)
+{
+    int result = -1;
+    if (number >= 0 && lseek(filesystem_fd, size_of_block * number + BLOCK_STATUS_OFFSET, SEEK_SET) >= 0)
+    {
+        char status;
+        result = read(filesystem_fd, &status, sizeof(char));
+        if (result < 0)
+        {
+            result = -1;
+        }
+        else if (result == 0)
+        {
+            result = BLOCK_STATUS_FREE;
+        }
+        else
+        {
+            result = status;
+        }
+    }
+    return result;
+}
+
+
+int get_inode_name(int number, char *buf)
+{
+    int result = -1;
+    if (number >= 0 && lseek(filesystem_fd, size_of_block * number + NODE_NAME_OFFSET, SEEK_SET) >= 0)
+    {
+        if (read(filesystem_fd, buf, NODE_NAME_MAX_SIZE) == NODE_NAME_MAX_SIZE)
+        {
+            result = 0;
+        }
+    }
+    return result;
+}
+
+
+int get_inode_stat(int number, stat_t *stbuf)
+{
+    int result = -1;
+    if (number >= 0 && lseek(filesystem_fd, size_of_block * number + NODE_STAT_OFFSET, SEEK_SET) >= 0)
+    {
+        if (read(filesystem_fd, stbuf, sizeof(stat_t)) == sizeof(stat_t))
+        {
+            result = 0;
+        }
+    }
+    return result;
+}
+
+
+int set_inode_name(int number, char *buf)
+{
+    int result = -1;
+    if (number >= 0 && lseek(filesystem_fd, size_of_block * number + NODE_NAME_OFFSET, SEEK_SET) >= 0)
+    {
+        if (write(filesystem_fd, buf, NODE_NAME_MAX_SIZE) == NODE_NAME_MAX_SIZE)
+        {
+            result = 0;
+        }
+    }
+    return result;
+}
+
+
+int set_inode_stat(int number, stat_t *buf)
+{
+    int result = -1;
+    if (number >= 0 && lseek(filesystem_fd, size_of_block * number + NODE_STAT_OFFSET, SEEK_SET) >= 0)
+    {
+        if (write(filesystem_fd, buf, sizeof(stat_t)) == sizeof(stat_t))
+        {
+            result = 0;
+        }
+    }
+    return result;
+}
+
+
+char *create_name(const char *name)
+{
+    char *result = (char *)calloc(NODE_NAME_MAX_SIZE, sizeof(char));
+    if (result != NULL)
+    {
+        int size = strlen(name) + 1;
+        if (size > NODE_NAME_MAX_SIZE)
+        {
+            size = NODE_NAME_MAX_SIZE;
+        }
+        memcpy(result, name, size);
+    }
+    return result;
+}
+
+
 char *create_empty_name()
 {
     return (char *)calloc(NODE_NAME_MAX_SIZE, sizeof(char));
@@ -193,4 +421,153 @@ char *create_empty_name()
 void destroy_name(char *name)
 {
     free(name);
+}
+
+
+char *exclude_last_node_name(char **node_names)
+{
+    char *result = NULL;
+    if (node_names != NULL && *node_names != NULL)
+    {
+        while (node_names[1] != NULL)
+        {
+            node_names++;
+        }
+        result = node_names[0];
+        node_names[0] = NULL;
+    }
+    return result;
+}
+
+
+void destroy_node_names(char **node_names)
+{
+    if (node_names != NULL)
+    {
+        char **tmp = node_names;
+        while (*tmp != NULL)
+        {
+            destroy_name(*tmp);
+            tmp++;
+        }
+        free(node_names);
+    }
+}
+
+
+int clear_block(int number)
+{
+    int result = -1;
+    if (number >= 0 && lseek(filesystem_fd, size_of_block * number, SEEK_SET) >= 0)
+    {
+        void *block = create_block();
+        if (block != NULL)
+        {
+            if (write_block(number, block) == 0)
+            {
+                result = 0;
+            }
+            destroy_block(block);
+        }
+    }
+    return result;
+}
+
+
+int add_inode_to_folder(int folder_number, int node_number)
+{
+    int result = -1;
+    if (folder_number >= 0 && node_number > 0)
+    {
+        inode_t *folder = (inode_t *)get_block(folder_number);
+        if (folder != NULL)
+        {
+            if (folder->status == BLOCK_STATUS_FOLDER)
+            {
+                int *start = (int *)folder->content;
+                int *end = (int *)((void *)folder + size_of_block);
+                while (start < end)
+                {
+                    if (*start <= 0)
+                    {
+                        *start = node_number;
+                        break;
+                    }
+                    start++;
+                }
+                if (start < end)
+                {
+                    result = write_block(folder_number, folder);
+                }
+            }
+            destroy_block(folder);
+        }
+    }
+    return result;
+}
+
+
+int remove_node_from_folder(int folder_number, int node_number)
+{
+    int result = -1;
+    if (folder_number >= 0 && node_number > 0)
+    {
+        inode_t *folder = (inode_t *)get_block(folder_number);
+        if (folder != NULL)
+        {
+            if (folder->status == BLOCK_STATUS_FOLDER)
+            {
+                int *start = (int *)folder->content;
+                int *end = (int *)((void *)folder + size_of_block);
+                while (start < end)
+                {
+                    if (*start == node_number)
+                    {
+                        *start = 0;
+                        break;
+                    }
+                    start++;
+                }
+                if (start < end)
+                {
+                    result = write_block(folder_number, folder);
+                }
+                else
+                {
+                    result = 0;
+                }
+            }
+            destroy_block(folder);
+        }
+    }
+    return result;}
+
+
+int search_inode_in_folder(int folder_number, const char *node_name)
+{
+    int result = -1;
+    if (folder_number >= 0 && node_name != NULL)
+    {
+        inode_t *folder = (inode_t *)get_block(folder_number);
+        if (folder != NULL)
+        {
+            if (folder->status == BLOCK_STATUS_FOLDER)
+            {
+                char name[NODE_NAME_MAX_SIZE];
+                int *start = (int *)folder->content;
+                int *end = (int *)((void *)folder + size_of_block);
+                while (start < end)
+                {
+                    if (*start > 0 && get_inode_name(*start, name) == 0 && strcmp(node_name, name) == 0)
+                    {
+                        result = *start;
+                        break;
+                    }
+                    start++;
+                }
+            }
+            destroy_block(folder);
+        }
+    }
+    return result;
 }
